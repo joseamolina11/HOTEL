7425ec0d555054f84cccf2572138bc89e2ca3986
andres-moli <andresmolinag2018@gmail.com>
2026-09-01 12:26:08 -0500

    fix

--- backend/src/modules/reports/reports.module.ts
+++ backend/src/modules/reports/reports.module.ts
@@ -9,9 +9,12 @@ import { FinancialMovement } from '../financial-movements/entities/financial-mov
 import { Payment } from '../payments/entities/payment.entity';
 import { Expense } from '../expenses/entities/expense.entity';
 import { PaymentMethod } from '../payment-methods/entities/payment-method.entity';
+import { Room } from '../rooms/entities/room.entity';
+import { Order } from '../orders/entities/order.entity';
+import { Consumption } from '../consumptions/entities/consumption.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Surcharge, CashRegister, FinancialMovement,Payment, Expense,PaymentMethod]), CashRegisterModule],
+  imports: [TypeOrmModule.forFeature([Surcharge, CashRegister, FinancialMovement,Payment, Expense,PaymentMethod, Room, Order,Consumption]), CashRegisterModule],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
